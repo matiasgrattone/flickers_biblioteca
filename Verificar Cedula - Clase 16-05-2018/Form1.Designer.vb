48b606cbea42b3aa7c9b87e4928635726c616839
LorenzoMenoni <ellolomenoni@gmail.com>
2018-05-17 08:23:32 -0300

    bto
    agre
    
    Co-Authored-By: lorenzomenoni <lorenzomenoni@users.noreply.github.com>
    Co-Authored-By: anonyg <anonyg@users.noreply.github.com>
    Co-Authored-By: agg383 <agg383@users.noreply.github.com>

--- Verificar Cedula - Clase 16-05-2018/Form1.Designer.vb
+++ Verificar Cedula - Clase 16-05-2018/Form1.Designer.vb
@@ -25,6 +25,7 @@ Partial Class Form1
         Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button2 = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -54,11 +55,21 @@ Partial Class Form1
         Me.DataGridView1.Size = New System.Drawing.Size(411, 132)
         Me.DataGridView1.TabIndex = 2
         '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(79, 75)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(145, 61)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(903, 447)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.WebBrowser1)
@@ -71,5 +82,6 @@ Partial Class Form1
     Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
