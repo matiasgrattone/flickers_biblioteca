5d6096c244e72552aa9cecd3185a66bd860849d7
AnonyG <Anony.G.Li.Ha@gmail.com>
2018-05-23 20:22:12 -0300

    Arreglos en login

--- Proyecto-Biblioteca/LOGIN.Designer.vb
+++ Proyecto-Biblioteca/LOGIN.Designer.vb
@@ -23,13 +23,13 @@ Partial Class LOGIN
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.contrasenia = New System.Windows.Forms.TextBox()
+        Me.usuario = New System.Windows.Forms.TextBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Panel1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -41,56 +41,23 @@ Partial Class LOGIN
         Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Controls.Add(Me.Label1)
         Me.Panel1.Controls.Add(Me.PictureBox1)
-        Me.Panel1.Controls.Add(Me.TextBox2)
-        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.contrasenia)
+        Me.Panel1.Controls.Add(Me.usuario)
         Me.Panel1.Controls.Add(Me.Panel2)
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(720, 331)
         Me.Panel1.TabIndex = 0
         '
-        'Panel2
+        'Button1
         '
-        Me.Panel2.BackColor = System.Drawing.Color.Lime
-        Me.Panel2.Location = New System.Drawing.Point(307, 43)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(3, 252)
-        Me.Panel2.TabIndex = 0
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(464, 97)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(213, 20)
-        Me.TextBox1.TabIndex = 1
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(464, 152)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(213, 20)
-        Me.TextBox2.TabIndex = 2
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Image = Global.Proyecto_Biblioteca.My.Resources.Resources._302c8eb1f942e4312e3dc37f61f468c6
-        Me.PictureBox1.Location = New System.Drawing.Point(57, 76)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(187, 190)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox1.TabIndex = 3
-        Me.PictureBox1.TabStop = False
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(332, 97)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(64, 20)
-        Me.Label1.TabIndex = 4
-        Me.Label1.Text = "Usuario"
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(464, 241)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(193, 54)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Iniciar Sesión"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Label2
         '
@@ -103,14 +70,50 @@ Partial Class LOGIN
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "Contraseña"
         '
-        'Button1
+        'Label1
         '
-        Me.Button1.Location = New System.Drawing.Point(464, 241)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(193, 54)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(332, 97)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Usuario"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Proyecto_Biblioteca.My.Resources.Resources._302c8eb1f942e4312e3dc37f61f468c6
+        Me.PictureBox1.Location = New System.Drawing.Point(57, 76)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(187, 190)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'contrasenia
+        '
+        Me.contrasenia.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.contrasenia.Location = New System.Drawing.Point(464, 152)
+        Me.contrasenia.Name = "contrasenia"
+        Me.contrasenia.Size = New System.Drawing.Size(213, 26)
+        Me.contrasenia.TabIndex = 2
+        '
+        'usuario
+        '
+        Me.usuario.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.usuario.Location = New System.Drawing.Point(464, 97)
+        Me.usuario.Name = "usuario"
+        Me.usuario.Size = New System.Drawing.Size(213, 26)
+        Me.usuario.TabIndex = 1
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Lime
+        Me.Panel2.Location = New System.Drawing.Point(307, 43)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(3, 252)
+        Me.Panel2.TabIndex = 0
         '
         'LOGIN
         '
@@ -129,8 +132,8 @@ Partial Class LOGIN
     Friend WithEvents Panel1 As System.Windows.Forms.Panel
     Friend WithEvents Panel2 As System.Windows.Forms.Panel
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents contrasenia As System.Windows.Forms.TextBox
+    Friend WithEvents usuario As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
--- Proyecto-Biblioteca/LOGIN.vb
+++ Proyecto-Biblioteca/LOGIN.vb
@@ -1,8 +1,9 @@
 ﻿Public Class LOGIN
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
-        inicio_usuarios.Label1.Text = TextBox1.Text
+        inicio_usuarios.Label1.Text = usuario.Text
         Me.Hide()
         inicio_usuarios.Show()
     End Sub
+
 End Class
