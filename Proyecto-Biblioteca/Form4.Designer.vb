2eef659458e98317a8265b446cf23779c7ea167a
matiasgrattone <matiasgrattone32@gmail.com>
2018-05-23 20:13:27 -0300

    Prueba
    hola mundo

--- Proyecto-Biblioteca/Form4.Designer.vb
+++ Proyecto-Biblioteca/Form4.Designer.vb
@@ -23,20 +23,24 @@ Partial Class Form4
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
-        Me.EXTRACCIONAGG = New System.Windows.Forms.ListBox()
         Me.LIBROSAGG = New System.Windows.Forms.ListBox()
         Me.devoCOMBO = New System.Windows.Forms.GroupBox()
-        Me.devoLIST = New System.Windows.Forms.ListBox()
+        Me.DataGridAGG = New System.Windows.Forms.DataGridView()
         Me.ExtCombo = New System.Windows.Forms.GroupBox()
         Me.Label10 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.LIBROS = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.VERLIBROSAGG = New System.Windows.Forms.DataGridView()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.ComboBoxMORTAL = New System.Windows.Forms.ComboBox()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.IDAGG = New System.Windows.Forms.ListBox()
@@ -46,75 +50,113 @@ Partial Class Form4
         Me.NOMBRE = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Cedula = New System.Windows.Forms.TextBox()
-        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.devoCOMBO.SuspendLayout()
+        CType(Me.DataGridAGG, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.ExtCombo.SuspendLayout()
         CType(Me.VERLIBROSAGG, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Label11
+        'Timer1
         '
-        Me.Label11.AutoSize = True
-        Me.Label11.Location = New System.Drawing.Point(257, 217)
-        Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(76, 13)
-        Me.Label11.TabIndex = 48
-        Me.Label11.Text = "EXTRACCION"
+        Me.Timer1.Enabled = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(940, 572)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(303, 83)
+        Me.Button4.TabIndex = 76
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(504, 575)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(288, 80)
+        Me.Button2.TabIndex = 75
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(261, 48)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(57, 21)
+        Me.Button1.TabIndex = 74
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(355, 390)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(102, 125)
+        Me.Panel1.TabIndex = 73
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(1126, 27)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 72
+        Me.Label4.Text = "Label4"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(1251, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 71
+        Me.Label2.Text = "Label2"
         '
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(142, 217)
+        Me.Label9.Location = New System.Drawing.Point(182, 221)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(39, 13)
-        Me.Label9.TabIndex = 47
+        Me.Label9.TabIndex = 70
         Me.Label9.Text = "LIBRO"
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(31, 217)
+        Me.Label8.Location = New System.Drawing.Point(71, 221)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(21, 13)
-        Me.Label8.TabIndex = 46
+        Me.Label8.TabIndex = 69
         Me.Label8.Text = "ID "
-        '
-        'EXTRACCIONAGG
-        '
-        Me.EXTRACCIONAGG.FormattingEnabled = True
-        Me.EXTRACCIONAGG.Location = New System.Drawing.Point(260, 233)
-        Me.EXTRACCIONAGG.Name = "EXTRACCIONAGG"
-        Me.EXTRACCIONAGG.Size = New System.Drawing.Size(118, 238)
-        Me.EXTRACCIONAGG.TabIndex = 45
         '
         'LIBROSAGG
         '
         Me.LIBROSAGG.FormattingEnabled = True
-        Me.LIBROSAGG.Location = New System.Drawing.Point(145, 233)
+        Me.LIBROSAGG.Location = New System.Drawing.Point(185, 237)
         Me.LIBROSAGG.Name = "LIBROSAGG"
         Me.LIBROSAGG.Size = New System.Drawing.Size(116, 238)
-        Me.LIBROSAGG.TabIndex = 44
+        Me.LIBROSAGG.TabIndex = 68
         '
         'devoCOMBO
         '
-        Me.devoCOMBO.Controls.Add(Me.devoLIST)
-        Me.devoCOMBO.Location = New System.Drawing.Point(916, 67)
+        Me.devoCOMBO.Controls.Add(Me.DataGridAGG)
+        Me.devoCOMBO.Location = New System.Drawing.Point(923, 71)
         Me.devoCOMBO.Name = "devoCOMBO"
         Me.devoCOMBO.Size = New System.Drawing.Size(413, 460)
-        Me.devoCOMBO.TabIndex = 43
+        Me.devoCOMBO.TabIndex = 67
         Me.devoCOMBO.TabStop = False
         Me.devoCOMBO.Text = "DEVOLUCION"
         '
-        'devoLIST
+        'DataGridAGG
         '
-        Me.devoLIST.FormattingEnabled = True
-        Me.devoLIST.Location = New System.Drawing.Point(20, 128)
-        Me.devoLIST.Name = "devoLIST"
-        Me.devoLIST.Size = New System.Drawing.Size(347, 316)
-        Me.devoLIST.TabIndex = 21
+        Me.DataGridAGG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridAGG.Location = New System.Drawing.Point(17, 130)
+        Me.DataGridAGG.Name = "DataGridAGG"
+        Me.DataGridAGG.Size = New System.Drawing.Size(390, 314)
+        Me.DataGridAGG.TabIndex = 0
         '
         'ExtCombo
         '
@@ -123,10 +165,10 @@ Partial Class Form4
         Me.ExtCombo.Controls.Add(Me.LIBROS)
         Me.ExtCombo.Controls.Add(Me.Label6)
         Me.ExtCombo.Controls.Add(Me.VERLIBROSAGG)
-        Me.ExtCombo.Location = New System.Drawing.Point(477, 67)
+        Me.ExtCombo.Location = New System.Drawing.Point(484, 71)
         Me.ExtCombo.Name = "ExtCombo"
         Me.ExtCombo.Size = New System.Drawing.Size(413, 460)
-        Me.ExtCombo.TabIndex = 42
+        Me.ExtCombo.TabIndex = 66
         Me.ExtCombo.TabStop = False
         Me.ExtCombo.Text = "EXTRACCION"
         '
@@ -172,69 +214,60 @@ Partial Class Form4
         Me.VERLIBROSAGG.Size = New System.Drawing.Size(376, 314)
         Me.VERLIBROSAGG.TabIndex = 17
         '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(832, 533)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(41, 25)
-        Me.Button1.TabIndex = 39
-        Me.Button1.Text = "GO"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
         'ComboBoxMORTAL
         '
         Me.ComboBoxMORTAL.FormattingEnabled = True
         Me.ComboBoxMORTAL.Items.AddRange(New Object() {"Extraccion", "Devolucion"})
-        Me.ComboBoxMORTAL.Location = New System.Drawing.Point(477, 25)
+        Me.ComboBoxMORTAL.Location = New System.Drawing.Point(484, 29)
         Me.ComboBoxMORTAL.Name = "ComboBoxMORTAL"
         Me.ComboBoxMORTAL.Size = New System.Drawing.Size(156, 21)
-        Me.ComboBoxMORTAL.TabIndex = 41
+        Me.ComboBoxMORTAL.TabIndex = 65
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(34, 622)
+        Me.Button3.Location = New System.Drawing.Point(41, 626)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(86, 29)
-        Me.Button3.TabIndex = 34
+        Me.Button3.TabIndex = 59
         Me.Button3.Text = "Moroso"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'IDAGG
         '
         Me.IDAGG.FormattingEnabled = True
-        Me.IDAGG.Location = New System.Drawing.Point(31, 233)
+        Me.IDAGG.Location = New System.Drawing.Point(71, 237)
         Me.IDAGG.Name = "IDAGG"
         Me.IDAGG.Size = New System.Drawing.Size(118, 238)
-        Me.IDAGG.TabIndex = 40
+        Me.IDAGG.TabIndex = 64
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(31, 81)
+        Me.Label3.Location = New System.Drawing.Point(38, 85)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(85, 13)
-        Me.Label3.TabIndex = 38
+        Me.Label3.TabIndex = 63
         Me.Label3.Text = "--------------------------"
         '
         'ESTADO
         '
         Me.ESTADO.AutoSize = True
         Me.ESTADO.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ESTADO.Location = New System.Drawing.Point(304, 112)
+        Me.ESTADO.Location = New System.Drawing.Point(311, 116)
         Me.ESTADO.Name = "ESTADO"
         Me.ESTADO.RightToLeft = System.Windows.Forms.RightToLeft.No
         Me.ESTADO.Size = New System.Drawing.Size(74, 24)
-        Me.ESTADO.TabIndex = 37
+        Me.ESTADO.TabIndex = 62
         Me.ESTADO.Text = "Estado"
         '
         'ENPODER
         '
         Me.ENPODER.AutoSize = True
         Me.ENPODER.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ENPODER.Location = New System.Drawing.Point(30, 176)
+        Me.ENPODER.Location = New System.Drawing.Point(102, 179)
         Me.ENPODER.Name = "ENPODER"
         Me.ENPODER.Size = New System.Drawing.Size(119, 20)
-        Me.ENPODER.TabIndex = 36
+        Me.ENPODER.TabIndex = 61
         Me.ENPODER.Text = "Libros en poder"
         '
         'NOMBRE
@@ -242,75 +275,52 @@ Partial Class Form4
         Me.NOMBRE.AutoSize = True
         Me.NOMBRE.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.NOMBRE.ForeColor = System.Drawing.Color.Black
-        Me.NOMBRE.Location = New System.Drawing.Point(29, 112)
+        Me.NOMBRE.Location = New System.Drawing.Point(36, 116)
         Me.NOMBRE.Name = "NOMBRE"
         Me.NOMBRE.Size = New System.Drawing.Size(87, 25)
-        Me.NOMBRE.TabIndex = 35
+        Me.NOMBRE.TabIndex = 60
         Me.NOMBRE.Text = "Nombre"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(27, 29)
+        Me.Label1.Location = New System.Drawing.Point(34, 33)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(196, 13)
-        Me.Label1.TabIndex = 33
+        Me.Label1.TabIndex = 58
         Me.Label1.Text = "INGRESE LA CEDULA DEL USUARIO "
         '
         'Cedula
         '
-        Me.Cedula.Location = New System.Drawing.Point(30, 45)
+        Me.Cedula.Location = New System.Drawing.Point(37, 49)
         Me.Cedula.Name = "Cedula"
         Me.Cedula.Size = New System.Drawing.Size(190, 20)
-        Me.Cedula.TabIndex = 32
+        Me.Cedula.TabIndex = 57
         '
-        'Timer1
+        'DataGridView1
         '
-        Me.Timer1.Enabled = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(1122, 593)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 49
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(1244, 25)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
-        Me.Label2.TabIndex = 50
-        Me.Label2.Text = "Label2"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(1119, 23)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(39, 13)
-        Me.Label4.TabIndex = 51
-        Me.Label4.Text = "Label4"
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(374, 430)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(36, 30)
+        Me.DataGridView1.TabIndex = 56
         '
         'Form4
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1370, 683)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Label11)
         Me.Controls.Add(Me.Label9)
         Me.Controls.Add(Me.Label8)
-        Me.Controls.Add(Me.EXTRACCIONAGG)
         Me.Controls.Add(Me.LIBROSAGG)
         Me.Controls.Add(Me.devoCOMBO)
         Me.Controls.Add(Me.ExtCombo)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.ComboBoxMORTAL)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.IDAGG)
@@ -320,30 +330,37 @@ Partial Class Form4
         Me.Controls.Add(Me.NOMBRE)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Cedula)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Name = "Form4"
         Me.Text = "Form4"
         Me.devoCOMBO.ResumeLayout(False)
+        CType(Me.DataGridAGG, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ExtCombo.ResumeLayout(False)
         Me.ExtCombo.PerformLayout()
         CType(Me.VERLIBROSAGG, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label9 As System.Windows.Forms.Label
     Friend WithEvents Label8 As System.Windows.Forms.Label
-    Friend WithEvents EXTRACCIONAGG As System.Windows.Forms.ListBox
     Friend WithEvents LIBROSAGG As System.Windows.Forms.ListBox
     Friend WithEvents devoCOMBO As System.Windows.Forms.GroupBox
-    Friend WithEvents devoLIST As System.Windows.Forms.ListBox
+    Friend WithEvents DataGridAGG As System.Windows.Forms.DataGridView
     Friend WithEvents ExtCombo As System.Windows.Forms.GroupBox
     Friend WithEvents Label10 As System.Windows.Forms.Label
     Friend WithEvents Label7 As System.Windows.Forms.Label
     Friend WithEvents LIBROS As System.Windows.Forms.TextBox
     Friend WithEvents Label6 As System.Windows.Forms.Label
     Friend WithEvents VERLIBROSAGG As System.Windows.Forms.DataGridView
-    Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents ComboBoxMORTAL As System.Windows.Forms.ComboBox
     Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents IDAGG As System.Windows.Forms.ListBox
@@ -353,8 +370,5 @@ Partial Class Form4
     Friend WithEvents NOMBRE As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Cedula As System.Windows.Forms.TextBox
-    Friend WithEvents Timer1 As System.Windows.Forms.Timer
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
 End Class
--- Proyecto-Biblioteca/Form4.vb
+++ Proyecto-Biblioteca/Form4.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form4
-   
+
     Private Sub Form4_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         '/////////////////////////////////////////////GRUPBOX OCULTOS////////////////////
         ExtCombo.Visible = False
@@ -20,7 +20,7 @@
         End If
     End Sub
 
-    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs)
 
         '////////////////////////////SE TOMA EL LIBRO MANDADO DEL TEXTBOX Y SE LO PASA A OCUPADO EN LA TABLA LIBROS///////////////////////  
 
@@ -32,7 +32,60 @@
 
     End Sub
 
-    Private Sub LIBROS_TextChanged(sender As System.Object, e As System.EventArgs) Handles LIBROS.TextChanged
+
+    Private Sub VERLIBROSAGG_CellContentClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles VERLIBROSAGG.CellContentClick
+
+        '////////////////////////////SI CEDULA.TEXT TIENE LA CEDULA PUESTA AHI SI SE PODRA AGREGAR LIBROS O REALIZAR OTRAS FUNCIONES  /////////////////////// 
+
+
+
+
+        IDAGG.Items.Add(VERLIBROSAGG.Item(0, VERLIBROSAGG.CurrentRow.Index).Value)
+        LIBROSAGG.Items.Add(VERLIBROSAGG.Item(1, VERLIBROSAGG.CurrentRow.Index).Value)
+
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+
+        Label2.Text = Date.Now.ToString("hh:mm:ss")
+
+        Label4.Text = Date.Now.ToString("yy / MM / dd")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs)
+
+    End Sub
+
+    Dim a As String
+    Private Sub Cedula_TextChanged(sender As System.Object, e As System.EventArgs) Handles Cedula.TextChanged
+
+        Consulta = "select cedula , nombre from usuarios where cedula like '" & Cedula.Text & "'  "
+        consultar()
+        DataGridView1.DataSource = Tabla
+
+
+
+
+
+
+        '  If lista <> 0 Then
+        'NOMBRE.Text = datos.Tables("usuarios").Rows(0).Item("Nombre")
+        ' ESTADO.Text = datos.Tables("usuarios").Rows(0).Item("observacion")
+        'End If
+
+        'If ESTADO.Text = "Moroso" Then
+        'ESTADO.BackColor = color.FromArgb(255, 0, 0)
+        '
+        'ElseIf ESTADO.Text <> "Moroso" Then
+        'ESTADO.BackColor = color.FromArgb(0, 255, 33)
+
+        ' End If
+    End Sub
+
+    Private Sub ComboBoxMORTAL_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ComboBoxMORTAL.SelectedIndexChanged
 
         '////////////////////////////SI EL COMBOBOX = EXTREACCION ----- SE MUESTRA EL GRUPOBOX1///////////////////////  
         If ComboBoxMORTAL.Text = "Extraccion" Then
@@ -50,36 +103,61 @@
         '////////////////////////////SI EL COMBOBOX = DEVOLUCION ----- SE MUESTRA EL GRUPOBOX2///////////////////////  
         If ComboBoxMORTAL.Text = "Devolucion" Then
             devoCOMBO.Visible = True
-
+            Consulta = "select * from prestamo where cedula = '" & DataGridView1.Item(0, DataGridView1.CurrentRow.Index).Value & "'" '// falta join
+            consultar()
+            DataGridAGG.DataSource = Tabla
         ElseIf ComboBoxMORTAL.Text <> "Devolucion" Then
             devoCOMBO.Visible = False
         End If
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
     End Sub
 
+    Private Sub Button1_Click_1(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        NOMBRE.Text = DataGridView1.Item(1, DataGridView1.CurrentRow.Index).Value
+    End Sub
+    Dim libro As String
+    Dim h As String
 
-    Private Sub VERLIBROSAGG_CellContentClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles VERLIBROSAGG.CellContentClick
+    Private Sub Button2_Click_1(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        libro = IDAGG.Items(0)
+        h = libro + " " + Cedula.Text + " " + Label4.Text
+        MsgBox(h)
+        Try
+            Consulta = "insert into prestamo  values ('" & Cedula.Text & "','" & libro & "','" & Label4.Text & "','')"
+            consultar()
 
-        If Cedula.Text <> "" Then '////////////////////////////SI CEDULA.TEXT TIENE LA CEDULA PUESTA AHI SI SE PODRA AGREGAR LIBROS O REALIZAR OTRAS FUNCIONES  /////////////////////// 
-            Dim I As Integer
-            I = VERLIBROSAGG.CurrentRow.Index
+            Consulta = "update libro set estado = 'ocupado' where cod_libro = '" & libro & "';"
+            consultar()
+            MsgBox("se ha ingresado")
 
-            IDAGG.Items.Add(VERLIBROSAGG.Item(0, I).Value())
-            LIBROSAGG.Items.Add(VERLIBROSAGG.Item(3, I).Value())
-            EXTRACCIONAGG.Items.Add(Label4.Text)
+
+
+        Catch ex As Exception
+            MsgBox("se ha ingresasdsddosdfasdfsdfsdfsdfsdfsdfs")
+        End Try
+
+    End Sub
+
+    Private Sub DataGridAGG_CellDoubleClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridAGG.CellContentClick
+        libro = DataGridAGG.Item(1, DataGridAGG.CurrentRow.Index).Value
+        Dim a As MsgBoxResult
+        a = MsgBox("Desea devolver el libro " & libro & " ?", MsgBoxStyle.YesNo)
+
+        If a = vbYes Then
+            Consulta = "update libro set estado = 'libre' where cod_libro = '" & libro & "';"
+            consultar()
+            MsgBox("se ha devuelto")
+            Consulta = "select * from prestamo where estado = ocupado"
+            consultar()
 
         End If
-    End Sub
 
-    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
-
-        Label2.Text = Date.Now.ToString("hh:mm:ss")
-
-        Label4.Text = Date.Now.ToString("dd / MM / yy")
-
-    End Sub
-
-    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        If a = vbNo Then
+            MsgBox("no")
+        End If
 
     End Sub
 End Class
