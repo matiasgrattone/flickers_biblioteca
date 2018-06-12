b776bd5f2e66b0af771d9feeae8dbec31cc0f2b9
Rexcapo3 <matiasgrattone32gmail.com>
2018-06-12 14:44:31 -0300

    ustedes lo piden , ustedes lo tienen
    desplazamiento de ventana

---
+++ Proyecto-Biblioteca/Menu2.vb
@@ -0,0 +1,92 @@
+﻿Public Class Menu2
+    Dim a As Integer = 0
+    Public xco, yco As Integer
+    Dim resultx, resulty As Integer
+    Dim xc, yc As Integer
+    Dim xf, yf As Integer
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Menu2_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        xf = Me.Location.X
+        yf = Me.Location.Y
+    End Sub
+
+    Private Sub Panel1_MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseDown
+
+        a = 1
+    End Sub
+
+
+    Private Sub Panel1_MouseUp(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseUp
+        xf = Me.Location.X
+        yf = Me.Location.Y
+        a = 0
+    End Sub
+
+
+    Private Sub Panel1_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseMove
+        If a = 1 Then
+            '///////////////////////////////////////////////////////MOUSE//////////////////////////////
+
+            
+
+            xc = Cursor.Position.X
+            yc = Cursor.Position.Y
+
+            Label3.Text = "X " & xc
+            Label4.Text = "Y " & yc
+
+            Label11.Text = "= " & xc - xco
+            Label12.Text = "= " & yc - yco
+
+            Dim holax, holay As Integer
+
+            holax = xc - xco
+            holay = yc - yco
+
+            Label11.Text = holax
+            Label12.Text = holay
+
+
+
+            '////////////////////////////////////////////////////////FORMULARIO //////////////////////
+
+
+           
+
+            Label1.Text = "X " & xf
+            Label2.Text = "Y " & yf
+
+
+            '/////////////////////////////////////////////////////////////////////////////////////////
+
+
+            ' MsgBox("x = " & resultx & "y " & resulty & "")
+
+
+            Me.Location = New Point(xf + holax, yf + holay)
+            'Me.Location = New Point(x - 431, y)
+
+
+
+        End If
+
+        If a = 0 Then
+
+
+
+
+            xco = Cursor.Position.X
+            yco = Cursor.Position.Y
+
+            Label9.Text = xco
+            Label10.Text = yco
+
+
+        End If
+
+    End Sub
+
+End Class
--- Proyecto-Biblioteca/My Project/Application.Designer.vb
+++ Proyecto-Biblioteca/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Proyecto_Biblioteca.LOGIN
+            Me.MainForm = Global.Proyecto_Biblioteca.Menu2
         End Sub
     End Class
 End Namespace
