67398d0037e9b5745a0764c2579f6f1bb0b19d92
LorenzoMenoni <ellolomenoni@gmail.com>
2018-09-06 08:29:20 -0300

    Implementacion FOrmulario ingreso revistas

--- Proyecto-Biblioteca/Betas/IngresoRevistas.Designer.vb
+++ Proyecto-Biblioteca/Betas/IngresoRevistas.Designer.vb
@@ -22,17 +22,122 @@ Partial Class IngresoRevistas
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.dgvrevistas = New System.Windows.Forms.DataGridView()
+        Me.btningresarrevista = New System.Windows.Forms.Button()
+        Me.lbltitulo = New System.Windows.Forms.Label()
+        Me.lblanio = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtdescripcion = New Proyecto_Biblioteca.PlaceHolder()
+        Me.txtorigenr = New Proyecto_Biblioteca.PlaceHolder()
+        Me.txtanior = New Proyecto_Biblioteca.PlaceHolder()
+        Me.txttitulo = New Proyecto_Biblioteca.PlaceHolder()
+        CType(Me.dgvrevistas, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'dgvrevistas
         '
-        Me.Button1.Location = New System.Drawing.Point(166, 92)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(473, 358)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.dgvrevistas.AllowUserToAddRows = False
+        Me.dgvrevistas.AllowUserToDeleteRows = False
+        Me.dgvrevistas.AllowUserToResizeColumns = False
+        Me.dgvrevistas.AllowUserToResizeRows = False
+        Me.dgvrevistas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvrevistas.Location = New System.Drawing.Point(12, 254)
+        Me.dgvrevistas.Name = "dgvrevistas"
+        Me.dgvrevistas.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
+        Me.dgvrevistas.RowHeadersVisible = False
+        Me.dgvrevistas.Size = New System.Drawing.Size(844, 320)
+        Me.dgvrevistas.TabIndex = 0
+        '
+        'btningresarrevista
+        '
+        Me.btningresarrevista.Location = New System.Drawing.Point(349, 195)
+        Me.btningresarrevista.Name = "btningresarrevista"
+        Me.btningresarrevista.Size = New System.Drawing.Size(122, 42)
+        Me.btningresarrevista.TabIndex = 1
+        Me.btningresarrevista.Text = "Ingresar"
+        Me.btningresarrevista.UseVisualStyleBackColor = True
+        '
+        'lbltitulo
+        '
+        Me.lbltitulo.AutoSize = True
+        Me.lbltitulo.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbltitulo.Location = New System.Drawing.Point(96, 63)
+        Me.lbltitulo.Name = "lbltitulo"
+        Me.lbltitulo.Size = New System.Drawing.Size(46, 15)
+        Me.lbltitulo.TabIndex = 6
+        Me.lbltitulo.Text = "Titulo:"
+        '
+        'lblanio
+        '
+        Me.lblanio.AutoSize = True
+        Me.lblanio.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblanio.Location = New System.Drawing.Point(105, 111)
+        Me.lblanio.Name = "lblanio"
+        Me.lblanio.Size = New System.Drawing.Size(37, 15)
+        Me.lblanio.TabIndex = 7
+        Me.lblanio.Text = "Año:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(442, 109)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 15)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Descripcion:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(428, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 15)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Pais de origen:"
+        '
+        'txtdescripcion
+        '
+        Me.txtdescripcion.Font = New System.Drawing.Font("Lucida Sans", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtdescripcion.Location = New System.Drawing.Point(534, 107)
+        Me.txtdescripcion.Multiline = True
+        Me.txtdescripcion.Name = "txtdescripcion"
+        Me.txtdescripcion.Size = New System.Drawing.Size(294, 76)
+        Me.txtdescripcion.TabIndex = 5
+        Me.txtdescripcion.WaterMarkColor = System.Drawing.Color.Gray
+        Me.txtdescripcion.WaterMarkText = "Ingrese Descripcion"
+        '
+        'txtorigenr
+        '
+        Me.txtorigenr.Font = New System.Drawing.Font("Lucida Sans", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtorigenr.Location = New System.Drawing.Point(534, 61)
+        Me.txtorigenr.Name = "txtorigenr"
+        Me.txtorigenr.Size = New System.Drawing.Size(173, 20)
+        Me.txtorigenr.TabIndex = 4
+        Me.txtorigenr.WaterMarkColor = System.Drawing.Color.Gray
+        Me.txtorigenr.WaterMarkText = "Pais de Origen"
+        '
+        'txtanior
+        '
+        Me.txtanior.Font = New System.Drawing.Font("Lucida Sans", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtanior.Location = New System.Drawing.Point(150, 109)
+        Me.txtanior.Name = "txtanior"
+        Me.txtanior.Size = New System.Drawing.Size(173, 20)
+        Me.txtanior.TabIndex = 3
+        Me.txtanior.WaterMarkColor = System.Drawing.Color.Gray
+        Me.txtanior.WaterMarkText = "Ingrese Año:"
+        '
+        'txttitulo
+        '
+        Me.txttitulo.Font = New System.Drawing.Font("Lucida Sans", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txttitulo.Location = New System.Drawing.Point(150, 61)
+        Me.txttitulo.Name = "txttitulo"
+        Me.txttitulo.Size = New System.Drawing.Size(173, 20)
+        Me.txttitulo.TabIndex = 2
+        Me.txttitulo.WaterMarkColor = System.Drawing.Color.Gray
+        Me.txttitulo.WaterMarkText = "Ingrese Titulo"
         '
         'IngresoRevistas
         '
@@ -40,12 +145,32 @@ Partial Class IngresoRevistas
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Chocolate
         Me.ClientSize = New System.Drawing.Size(868, 586)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblanio)
+        Me.Controls.Add(Me.lbltitulo)
+        Me.Controls.Add(Me.txtdescripcion)
+        Me.Controls.Add(Me.txtorigenr)
+        Me.Controls.Add(Me.txtanior)
+        Me.Controls.Add(Me.txttitulo)
+        Me.Controls.Add(Me.btningresarrevista)
+        Me.Controls.Add(Me.dgvrevistas)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "IngresoRevistas"
         Me.Text = "IngresoRevistas"
+        CType(Me.dgvrevistas, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents dgvrevistas As System.Windows.Forms.DataGridView
+    Friend WithEvents btningresarrevista As System.Windows.Forms.Button
+    Friend WithEvents txttitulo As Proyecto_Biblioteca.PlaceHolder
+    Friend WithEvents txtanior As Proyecto_Biblioteca.PlaceHolder
+    Friend WithEvents txtorigenr As Proyecto_Biblioteca.PlaceHolder
+    Friend WithEvents txtdescripcion As Proyecto_Biblioteca.PlaceHolder
+    Friend WithEvents lbltitulo As System.Windows.Forms.Label
+    Friend WithEvents lblanio As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 End Class
--- Proyecto-Biblioteca/Betas/IngresoRevistas.vb
+++ Proyecto-Biblioteca/Betas/IngresoRevistas.vb
@@ -1,6 +1,27 @@
 ﻿Public Class IngresoRevistas
 
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        MsgBox("Hola Mundo")
+    Private Sub IngresoRevistas_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Consulta = "select titulo,anio,origen,descripcion from revistas"
+        consultar()
+        dgvrevistas.DataSource = Tabla
+        DatagridModulo = dgvrevistas
+        Datagrid_Align()
+        dgvrevistas.Columns(0).HeaderText = "Titulo"
+        dgvrevistas.Columns(1).HeaderText = "Año"
+        dgvrevistas.Columns(2).HeaderText = "Pais de Origen"
+        dgvrevistas.Columns(3).HeaderText = "Descripcion"
+
+    End Sub
+
+    Private Sub btningresarrevista_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btningresarrevista.Click
+        Consulta = "insert into revistas(titulo,anio,origen,descripcion) values ('" & txttitulo.Text & "', '" & txtanior.Text & "','" + txtorigenr.Text + "','" + txtdescripcion.Text + "')"
+        consultar()
+
+
+        Consulta = "select titulo,anio,origen,descripcion from revistas"
+        consultar()
+        dgvrevistas.DataSource = Tabla
+        DatagridModulo = dgvrevistas
+        Datagrid_Align()
     End Sub
 End Class
