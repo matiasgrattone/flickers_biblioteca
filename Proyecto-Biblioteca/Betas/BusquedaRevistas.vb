741005b0d0d50db5c8b3016cf71dbd2ebc9b74ca
Guille Coelho <guillecoe2015@gmail.com>
2018-09-12 18:27:51 -0300

    MODIFICAR REVISTAS CASI LISTO!

--- Proyecto-Biblioteca/Betas/BusquedaRevistas.vb
+++ Proyecto-Biblioteca/Betas/BusquedaRevistas.vb
@@ -37,35 +37,55 @@
     End Sub
 
     Private Sub BusquedaRevistas_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'Comprobacion'
+
+        '--------------------Comprobacion de si la base de datos funciona---------------------
         Consulta = "select * from usuarios"
         consultar()
-        dgvcomprobar.DataSource = Tabla
-        Try
-            If dgvcomprobar.Item(0, dgvcomprobar.CurrentRow.Index).Value <> 0 Then
+        For Each row As DataRow In Tabla.Rows
+            If row("nombre") <> "" Then
                 bandera = 1
             Else
                 bandera = 0
             End If
-        Catch ex1 As Exception
-            MsgBox("Base de datos no disponible")
-        End Try
-
+        Next
+        '-------------------------------------------------------------------------------------
 
         If bandera = 1 Then
             Dim contadoringreso As Integer = 0
-            Consulta = "select titulo,anio,origen,descripcion,estado from revistas"
+            Consulta = "select * from revistas order by id_revistas ASC"
             consultar()
             dgvrevistas.DataSource = Tabla
             DatagridModulo = dgvrevistas
             Datagrid_Align()
-            dgvrevistas.Columns(0).HeaderText = "Titulo"
-            dgvrevistas.Columns(1).HeaderText = "Año"
-            dgvrevistas.Columns(2).HeaderText = "Pais de Origen"
-            dgvrevistas.Columns(3).HeaderText = "Descripcion"
+            dgvrevistas.Columns(0).HeaderText = "ID"
+            dgvrevistas.Columns(1).HeaderText = "Titulo"
+            dgvrevistas.Columns(2).HeaderText = "Año"
+            dgvrevistas.Columns(3).HeaderText = "Pais de Origen"
             dgvrevistas.Columns(4).HeaderText = "Estado"
+            dgvrevistas.Columns(5).HeaderText = "Descripcion"
+
             cmbestado.SelectedIndex = 0
             cmbdatos.SelectedIndex = 0
+
+            lblid.Text = dgvrevistas.Item(0, 0).Value
+            lbltitulomostrar.Text = dgvrevistas.Item(1, 0).Value
+            lblaniomostrar.Text = dgvrevistas.Item(2, 0).Value
+            lblorigenmostrar.Text = dgvrevistas.Item(3, 0).Value
+            If IsDBNull(dgvrevistas.Item(5, 0).Value) Then
+                txtdescmostrar.Text = "Descripcion vacia"
+            Else
+                txtdescmostrar.Text = dgvrevistas.Item(5, 0).Value
+            End If
+            Select Case dgvrevistas.Item(4, 0).Value
+                Case 0
+                    estado_label.Text = "Disponible"
+                Case 1
+                    estado_label.Text = "Ocupado"
+                Case 2
+                    estado_label.Text = "Reservado"
+                Case 3
+                    estado_label.Text = "Discontinuado"
+            End Select
 
         Else
             titulo_txt.ReadOnly = True
@@ -73,6 +93,7 @@
             anio_txt.ReadOnly = True
             desc_txt.ReadOnly = True
         End If
+
     End Sub
 
     Private Sub btnmodificar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnmodificar.Click
@@ -81,12 +102,11 @@
 
         If bandera = 1 Then
             Try
+                id_txt.Text = lblid.Text
                 titulo_txt.Text = lbltitulomostrar.Text
                 anio_txt.Text = lblaniomostrar.Text
                 origen_txt.Text = lblorigenmostrar.Text
                 desc_txt.Text = txtdescmostrar.Text
-
-
 
                 'Permite seleccionar el estado predeterminado del combobox al momento de modificar los datos'
                 '0-disponible,1-ocupado, 2-reservado,3-descontinuado'
@@ -112,39 +132,31 @@
         End If
     End Sub
 
-    Private Sub dgvrevistas_CellDoubleClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgvrevistas.CellDoubleClick
-
-        Consulta = "select * from revistas"
-        consultar()
-        For Each row As DataRow In Tabla.Rows
-            titulo_txt.Text = row("titulo").ToString
-            anio_txt.Text = row("anio").ToString
-            origen_txt.Text = row("origen").ToString
-            desc_txt.Text = row("descripcion").ToString
-            Select Case row("estado")
-                Case 0
-                    cmbupdate.SelectedIndex = 0
-                Case 1
-                    cmbupdate.SelectedIndex = 1
-                Case 2
-                    cmbupdate.SelectedIndex = 2
-                Case 3
-                    cmbupdate.SelectedIndex = 3
-            End Select
-        Next
-
-    End Sub
-
-    Private Sub dgvrevistas_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles dgvrevistas.MouseClick
+    Private Sub dgvrevistas_CellContentClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgvrevistas.CellContentClick
+        id = dgvrevistas.Item(0, dgvrevistas.CurrentRow.Index).Value
         Try
             '///El usuario selecciona una fila de datos en el DataGrid y estos son copiados a un Panel///'
             'Permite al usuario una mejor visualizacion de los datos del libro que busca'
-            lbltitulomostrar.Text = dgvrevistas.Item(0, dgvrevistas.CurrentRow.Index).Value
-            lblaniomostrar.Text = dgvrevistas.Item(1, dgvrevistas.CurrentRow.Index).Value
-            lblorigenmostrar.Text = dgvrevistas.Item(2, dgvrevistas.CurrentRow.Index).Value
-            txtdescmostrar.Text = dgvrevistas.Item(3, dgvrevistas.CurrentRow.Index).Value
-
-            estado_label.Text = dgvrevistas.Item(4, dgvrevistas.CurrentRow.Index).Value
+            lblid.Text = dgvrevistas.Item(0, dgvrevistas.CurrentRow.Index).Value
+            lbltitulomostrar.Text = dgvrevistas.Item(1, dgvrevistas.CurrentRow.Index).Value
+            lblaniomostrar.Text = dgvrevistas.Item(2, dgvrevistas.CurrentRow.Index).Value
+            lblorigenmostrar.Text = dgvrevistas.Item(3, dgvrevistas.CurrentRow.Index).Value
+            If IsDBNull(dgvrevistas.Item(5, dgvrevistas.CurrentRow.Index).Value) Then
+                txtdescmostrar.Text = "Descripcion vacia"
+            Else
+                txtdescmostrar.Text = dgvrevistas.Item(5, dgvrevistas.CurrentRow.Index).Value
+            End If
+            'estado_label.Text = dgvrevistas.Item(4, dgvrevistas.CurrentRow.Index).Value
+            Select Case dgvrevistas.Item(4, dgvrevistas.CurrentRow.Index).Value
+                Case 0
+                    estado_label.Text = "Disponible"
+                Case 1
+                    estado_label.Text = "Ocupado"
+                Case 2
+                    estado_label.Text = "Reservado"
+                Case 3
+                    estado_label.Text = "Discontinuado"
+            End Select
         Catch ex As Exception
 
         End Try
@@ -251,6 +263,10 @@
         anio_txt.Clear()
         origen_txt.Clear()
         desc_txt.Clear()
+        ErrorProvider1.SetError(titulo_txt, "")
+        ErrorProvider1.SetError(anio_txt, "")
+        ErrorProvider1.SetError(origen_txt, "")
+
         cmbupdate.BringToFront()
         Pactualizar.SendToBack()
     End Sub
@@ -307,8 +323,12 @@
         '------------Se actualizan los datos en la BD------------
         If errorr = 0 Then
             Try
-                Consulta = "update table revistas set titulo='" & titulo & "' where id_revistas='" & id & "'"
+                Consulta = "update revistas set titulo='" & titulo & "', anio='" & Str(año) & "', origen='" & origen & "', descripcion='" & descripcion & "', estado='" & Str(estado) & "' where id_revistas='" & id_txt.Text & "'"
                 consultar()
+                MsgBox("Se actualizaron los datos correctamente")
+                Consulta = "select * from revistas"
+                consultar()
+                dgvrevistas.DataSource = Tabla
             Catch ex As Exception
                 MsgBox("No se pudo actualizar la informacion")
             End Try
@@ -318,4 +338,21 @@
 
     End Sub
 
+    Private Sub anio_txt_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles anio_txt.TextChanged
+        anio_txt.MaxLength = 4
+        If Not IsNumeric(anio_txt.Text) Then
+            anio_txt.Clear()
+            ErrorProvider1.SetError(anio_txt, "Escriba solo numeros")
+        Else
+            ErrorProvider1.SetError(anio_txt, "")
+        End If
+    End Sub
+
+    Private Sub titulo_txt_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles titulo_txt.TextChanged
+        ErrorProvider1.SetError(titulo_txt, "")
+    End Sub
+
+    Private Sub origen_txt_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles origen_txt.TextChanged
+        ErrorProvider1.SetError(origen_txt, "")
+    End Sub
 End Class
