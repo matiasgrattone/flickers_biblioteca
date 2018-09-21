fead91edf3ae4c39dd4e45e07cebb1db02f40ee1
Guille Coelho <guillecoe2015@gmail.com>
2018-09-20 23:00:52 -0300

    FIXEO
    -Solucionado el error de desbordamineto en verificar cedula

--- Proyecto-Biblioteca/Modulo/Modulo.vb
+++ Proyecto-Biblioteca/Modulo/Modulo.vb
@@ -23,8 +23,8 @@ Module Modulo
 
         If invitado = 0 Then
             'ubicacion = "server=localhost; user id=root; password=''; database=biblioteca"
-            'ubicacion = "server=192.168.1.12; user id=admin; password=admin; database=biblioteca" ' NO BORRAR, YO USO ESTA PARA PROGRAMAR EN CASA, GUILLE (NO PUEDO USAR EL DOMINIO EN LAN)
-            ubicacion = "server=bibliotecadb.ddns.net; user id=admin; password=admin; database=biblioteca"
+            ubicacion = "server=192.168.1.12; user id=admin; password=admin; database=biblioteca" ' NO BORRAR, YO USO ESTA PARA PROGRAMAR EN CASA, GUILLE (NO PUEDO USAR EL DOMINIO EN LAN)
+            'ubicacion = "server=bibliotecadb.ddns.net; user id=admin; password=admin; database=biblioteca"
         Else
             'ubicacion = "server=192.168.1.12; user id=invitado; password=invitado; database=biblioteca"
             ubicacion = "server=bibliotecadb.ddns.net; user id=invitado; password=invitado; database=biblioteca"
@@ -174,7 +174,7 @@ Module Modulo
 
             For i = 0 To 6
                 Dim num1 As Integer = Val(num(i))
-                suma = (num1 * calculo(i)) & suma
+                suma = (num1 * calculo(i)) + suma
             Next
 
             num = cedula.ToCharArray()
