ba66de11ce3fca458fd8251fdd6e95de8d9efe21
LorenzoMenoni <39371478+LorenzoMenoni@users.noreply.github.com>
2018-10-16 08:18:33 -0300

    Finalizacion Formulario Registro Revistas
    formulario completado

--- Proyecto-Biblioteca/My Project/Application.Designer.vb
+++ Proyecto-Biblioteca/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Proyecto_Biblioteca.Seleccion_Libro
+            Me.MainForm = Global.Proyecto_Biblioteca.registrorevistas
         End Sub
     End Class
 End Namespace
